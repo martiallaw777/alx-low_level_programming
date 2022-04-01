7e6377990ce9836fd03ad26cea9630ad833e2c17
martiallaw777 <100254945+martiallaw777@users.noreply.github.com>
2022-04-02 00:33:08 +0100

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,20 @@
-rwe
+#include "main.h"
+/**
+ *_strcpy - copies pointed string
+ *@dest: dest
+ *@src: src
+ *
+ *Return: pointer to dest
+ **/
+char *_strcpy(char *dest, char *src)
+{
+char  *start = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
+}
