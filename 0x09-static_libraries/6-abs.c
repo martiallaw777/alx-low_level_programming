b75de23845d18a9fe4089ef873eacfc7f13d873c
martiallaw777 <100254945+martiallaw777@users.noreply.github.com>
2022-04-02 00:32:29 +0100

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,16 @@
-erwv
+#include "main.h"
+#include <stdio.h>
+/**
+* _abs - check positive, negative or zero
+* @n: the number to be checked
+*
+* Return: return absolute value
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (n * -1);
+}
