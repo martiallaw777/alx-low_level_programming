9c2ff07aabb1df72057570dabffe39dea3685cd1
martiallaw777 <100254945+martiallaw777@users.noreply.github.com>
2022-04-02 00:14:44 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,16 @@
-fdsf
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - return lenght of string
+ *@s: pointer s
+ *Return: i
+ **/
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
